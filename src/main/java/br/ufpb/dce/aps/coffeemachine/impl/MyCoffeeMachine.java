c60c7b9eef9432d7aed6d7f1640905dc91cd00bd
Wennio Kelson <wennio.kelson@dce.ufpb.br>
2014-08-19 09:33:23 -0300

    Task 1.19 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -9,46 +9,24 @@ import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 import br.ufpb.dce.aps.coffeemachine.Drink;
 import br.ufpb.dce.aps.coffeemachine.Messages;
 
-public class MyCoffeeMachine implements CoffeeMachine{
-	protected int total ; 
-	protected ComponentsFactory fac;
-	protected ArrayList<Coin> coins, listaTroco;
-	protected Coin[] aux = new Coin[2];
-	protected int cafe = 35;
+public class MyCoffeeMachine implements CoffeeMachine {
+	
+	private ComponentsFactory fac;
+	private int total, cafe;
+	private ArrayList<Coin> coins, listaTroco;
 	
 	public MyCoffeeMachine(ComponentsFactory factory) {
 		fac = factory;
 		fac.getDisplay().info("Insert coins and select a drink!");
 		coins = new ArrayList<Coin>();
 		listaTroco = new ArrayList<Coin>();
-	}
-
-	public void insertCoin(Coin coin) {
-		if (coin == null) {
-			throw new CoffeeMachineException("Insert null coin");
-		}
-		total += coin.getValue();
-		fac.getDisplay().info("Total: US$ " + total/100 + "." + total%100);
-		coins.add(coin);
-		
-	}
-
-	public void cancel() {
-		if (this.total == 0) {
-			throw new CoffeeMachineException(" Cancel without inserting coins");
-		}
-		fac.getDisplay().warn(Messages.CANCEL);
-		removerMoeda();	
-		fac.getDisplay().info("Insert coins and select a drink!");
-			
-		
-
-		
+		cafe = 35;
 	}
 	
-	public void removerMoeda(){
+	//Meus métodos
+	
+	private void retornarMoedas(){
 		Coin[] reverso = Coin.reverse();
-		
 		for (Coin r : reverso) {
 			for (Coin aux : this.coins) {
 				if (aux == r) {
@@ -57,23 +35,46 @@ public class MyCoffeeMachine implements CoffeeMachine{
 			}
 		}
 	}
+		
+	private void tarefaCompletaDevolverMoedas(){
+		fac.getDisplay().warn("Cancelling drink. Please, get your coins.");
+		this.retornarMoedas();
+		fac.getDisplay().info("Insert coins and select a drink!");
+	}
 	
-	public int calculaTroco() {
+	public void blackPlan(){
+		fac.getCupDispenser().contains(1);
+		fac.getWaterDispenser().contains(1.1);
+		fac.getCoffeePowderDispenser().contains(1.2);
+	}
+	
+	public void blackMix(){
+		fac.getDisplay().info(Messages.MIXING);
+		fac.getCoffeePowderDispenser().release(1.3);
+		fac.getWaterDispenser().release(1.4);
+	}
+	
+	public void drinkRelease(){
+		fac.getDisplay().info(Messages.RELEASING);
+		fac.getCupDispenser().release(1);
+		fac.getDrinkDispenser().release(1.5);
+		fac.getDisplay().info(Messages.TAKE_DRINK);
+		fac.getDisplay().info(Messages.INSERT_COINS);
+	}
+	
+	public int calculaTroco(){
 		int contador = 0;
-		for (Coin c : this.coins) {
+		for(Coin c : this.coins){
 			contador += c.getValue();
 		}
 		return (contador - this.cafe);
-
 	}
 	
 	public boolean planejamento(int troco) {
 		for (Coin coin : Coin.reverse()) {
 			if (coin.getValue() <= troco && this.fac.getCashBox().count(coin) > 0) {
-				
 				troco -= coin.getValue();
 			}
-
 		}
 		return troco == 0;
 	}
@@ -88,114 +89,98 @@ public class MyCoffeeMachine implements CoffeeMachine{
 		}
 		return listaTroco;
 	}
+	
+	//Métodos do teste
+	
+	public void insertCoin(Coin moeda) {
+		if(moeda == null){
+			throw new CoffeeMachineException("Insert null coin");
+		}
+		total += moeda.getValue();
+		fac.getDisplay().info("Total: US$ " + total/100 + "." + total%100);
+		coins.add(moeda);
+	}
 
+	public void cancel() {
+		if (this.total == 0) {
+			throw new CoffeeMachineException(" Cancel without inserting coins");
+		}else if(coins.size() >= 1){
+			this.tarefaCompletaDevolverMoedas();
+		}
+	}
+	
 	public void select(Drink drink) {
-		if(Drink.BLACK == drink){
-			if(!(fac.getCupDispenser().contains(1))){
-				fac.getDisplay().warn(Messages.OUT_OF_CUP);
-				removerMoeda();
-				fac.getDisplay().info("Insert coins and select a drink!");
-				return;
-			}
+		if(total < this.cafe){
+			fac.getDisplay().warn(Messages.NO_ENOUGHT_MONEY);
+			retornarMoedas();
+			fac.getDisplay().info(Messages.INSERT_COINS);
+			return;
+		}
+		switch (drink) {
+		case BLACK:
+			fac.getCupDispenser().contains(1);
 			if(!(fac.getWaterDispenser().contains(3.0))){
 				fac.getDisplay().warn(Messages.OUT_OF_WATER);
-				removerMoeda();
-				fac.getDisplay().info("Insert coins and select a drink!");
+				retornarMoedas();
+				fac.getDisplay().info(Messages.INSERT_COINS);
 				return;
 			}
 			if(!(fac.getCoffeePowderDispenser().contains(3.0))){
 				fac.getDisplay().warn(Messages.OUT_OF_COFFEE_POWDER);
-				removerMoeda();
-				fac.getDisplay().info("Insert coins and select a drink!");
+				retornarMoedas();
+				fac.getDisplay().info(Messages.INSERT_COINS);
 				return;
 			}
-				
-			fac.getDisplay().info("Mixing ingredients.");
-			fac.getCoffeePowderDispenser().release(3.0);
-				
-			fac.getWaterDispenser().release(3.0);
-			fac.getDisplay().info("Releasing drink.");
-				
-			fac.getCupDispenser().release(1);
-			fac.getDrinkDispenser().release(3.0);
-			fac.getDisplay().info("Please, take your drink.");
-			fac.getDisplay().info("Insert coins and select a drink!");
-				
-		}
-		else if(Drink.BLACK_SUGAR == drink){
-				
+			this.blackMix();	
+			this.drinkRelease();
+			break;
+		case BLACK_SUGAR:
 			if(!(fac.getCupDispenser().contains(1))){
 				fac.getDisplay().warn(Messages.OUT_OF_CUP);
-				removerMoeda();
-				fac.getDisplay().info("Insert coins and select a drink!");
+				retornarMoedas();
+				fac.getDisplay().info(Messages.INSERT_COINS);
 				return;
 			}
-			fac.getWaterDispenser().contains(3.0);
-			fac.getCoffeePowderDispenser().contains(3.0);
-			if(!(fac.getSugarDispenser().contains(1.0))){
+			fac.getWaterDispenser().contains(1.1);
+			fac.getCoffeePowderDispenser().contains(1.2);
+			if(!(fac.getSugarDispenser().contains(2.1))){
 				fac.getDisplay().warn(Messages.OUT_OF_SUGAR);
-				removerMoeda();
-				fac.getDisplay().info("Insert coins and select a drink!");
+				retornarMoedas();
+				fac.getDisplay().info(Messages.INSERT_COINS);
 				return;
 			}
-				
-		
-			fac.getDisplay().info("Mixing ingredients.");
-			fac.getCoffeePowderDispenser().release(3.0);
-			fac.getWaterDispenser().release(3.0);
-			fac.getSugarDispenser().release(2.0);
-			fac.getDisplay().info("Releasing drink.");
-				
-			fac.getCupDispenser().release(1);
-			fac.getDrinkDispenser().release(3.0);
-			fac.getDisplay().info("Please, take your drink.");
-			fac.getDisplay().info("Insert coins and select a drink!");
-			
-				
+			this.blackMix();
+			fac.getSugarDispenser().release(2.2);
+			this.drinkRelease();
 			this.coins.clear();
-				
-		}
-		else if(Drink.WHITE == drink){
-			fac.getCupDispenser().contains(1);
-			fac.getWaterDispenser().contains(3.0);
-			fac.getCoffeePowderDispenser().contains(3.0);
-			fac.getCreamerDispenser().contains(3.0);
-			
-			fac.getDisplay().info(Messages.MIXING);
-			fac.getCoffeePowderDispenser().release(3.0);
-			fac.getWaterDispenser().release(3.0);
-			fac.getCreamerDispenser().release(3.0);
-			
+			break;
+		case WHITE:
+			this.blackPlan();
+			fac.getCreamerDispenser().contains(2.1);
+			this.blackMix();
+			fac.getCreamerDispenser().release(2.2);
+			this.drinkRelease();
+			break;
+		case WHITE_SUGAR:
+			this.blackPlan();
+			fac.getCreamerDispenser().contains(2.1);
+			fac.getSugarDispenser().contains(3.1);
+			this.planejamento(this.calculaTroco());
+			this.blackMix();
+			fac.getCreamerDispenser().release(2.2);
+			fac.getSugarDispenser().release(3.2);
 			fac.getDisplay().info(Messages.RELEASING);
 			fac.getCupDispenser().release(1);
-			fac.getDrinkDispenser().release(3.0);
+			fac.getDrinkDispenser().release(1.5);
 			fac.getDisplay().info(Messages.TAKE_DRINK);
-			
+			this.retornarTroco(this.calculaTroco());
 			fac.getDisplay().info(Messages.INSERT_COINS);
-		}
-		else if (Drink.WHITE_SUGAR == drink) {
-			fac.getCupDispenser().contains(1);
-			fac.getWaterDispenser().contains(3.0);
-			fac.getCoffeePowderDispenser().contains(3.0);
-			fac.getCreamerDispenser().contains(3.0);
-			fac.getSugarDispenser().contains(3.0);
-
-			planejamento(calculaTroco());
-			
-			fac.getDisplay().info(Messages.MIXING);
-			fac.getCoffeePowderDispenser().release(3.0);
-			fac.getWaterDispenser().release(3.0);
-			fac.getCreamerDispenser().release(3.0);
-			fac.getSugarDispenser().release(3.0);
-			
-			fac.getDisplay().info(Messages.RELEASING);
-			fac.getCupDispenser().release(1);
-			fac.getDrinkDispenser().release(3.0);
-			fac.getDisplay().info(Messages.TAKE_DRINK);
-			
-			retornarTroco(calculaTroco());
-			
-			fac.getDisplay().info(Messages.INSERT_COINS);
+			break;
+		default:
+			break;
 		}
 	}
+	
+
+	
 }
